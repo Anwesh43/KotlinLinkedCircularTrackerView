e30aee5db1177523c94c0bf9a37ed923b3bf1992
Anwesh43 <anweshthecool0@gmail.com>
2018-06-24 02:32:08 +0530

    adding LinkedCircularTrackerView class

--- linkedcirculartrackerview/src/main/java/com/anwesh/uiprojects/linkedcirculartrackerview/LinkedCircularTrackerView.kt
+++ linkedcirculartrackerview/src/main/java/com/anwesh/uiprojects/linkedcirculartrackerview/LinkedCircularTrackerView.kt
@@ -1,5 +1,30 @@
 package com.anwesh.uiprojects.linkedcirculartrackerview
-
 /**
  * Created by anweshmishra on 24/06/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val CT_NODES : Int = 5
+
+class LinkedCircularTrackerView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+        
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
