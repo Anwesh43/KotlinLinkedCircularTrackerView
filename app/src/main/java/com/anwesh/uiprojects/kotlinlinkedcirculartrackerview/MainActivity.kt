e30dbf7f9bbf78e17fa1d675bf30796f25fd187a
Anwesh43 <anweshthecool0@gmail.com>
2018-06-24 03:01:48 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirculartrackerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirculartrackerview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedcirculartrackerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedcirculartrackerview.LinkedCircularTrackerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedCircularTrackerView.create(this)
     }
 }
