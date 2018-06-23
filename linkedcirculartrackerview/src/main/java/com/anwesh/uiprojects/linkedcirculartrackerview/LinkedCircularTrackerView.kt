9d87236ab2d699acf05d205ac7a5e7211c0dc60b
Anwesh43 <anweshthecool0@gmail.com>
2018-06-24 02:36:49 +0530

    adding CTState class

--- linkedcirculartrackerview/src/main/java/com/anwesh/uiprojects/linkedcirculartrackerview/LinkedCircularTrackerView.kt
+++ linkedcirculartrackerview/src/main/java/com/anwesh/uiprojects/linkedcirculartrackerview/LinkedCircularTrackerView.kt
@@ -16,7 +16,7 @@ class LinkedCircularTrackerView (ctx : Context) : View(ctx) {
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-        
+
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
@@ -26,5 +26,31 @@ class LinkedCircularTrackerView (ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class CTState (var prevScale : Float = 0f, var j : Int = 0, var dir : Float = 0f) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j -= dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
     }
 }
