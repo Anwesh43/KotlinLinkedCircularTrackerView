3e1833b5cb3d5de5de3ec52b7c2731d85d67986e
Anwesh43 <anweshthecool0@gmail.com>
2018-06-24 11:06:23 +0530

    addig StepCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirculartrackerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedcirculartrackerview/MainActivity.kt
@@ -3,14 +3,18 @@ package com.anwesh.uiprojects.kotlinlinkedcirculartrackerview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedcirculartrackerview.LinkedCircularTrackerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedCircularTrackerView.create(this)
+        val view : LinkedCircularTrackerView = LinkedCircularTrackerView.create(this)
         fullScreen()
+        view.addOnStepCompletionListener {
+            Toast.makeText(this, "${it} is completed" ,Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
